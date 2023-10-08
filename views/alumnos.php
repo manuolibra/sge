8c6e648c0244ff808615a99bce8aa5c1df79a3de
gabospacecowboy <josegabovillalobos@gmail.com>
2023-10-08 18:12:38 -0400

    alummnos

--- views/alumnos.php
+++ views/alumnos.php
@@ -9,7 +9,7 @@
         <!-- DataTales Example -->
         <div class="card shadow mb-4">
             <div class="card-header py-3">
-                <h6 class="m-0 font-weight-bold text-primary">Lista de Alumnos</h6>
+                <h6 class="m-0 font-weight-bold text-primary">Alumnos</h6>
                 <br>
 
                 <button type="button" class="btn btn-success" data-toggle="modal" data-target="#alumno">
